5e12a5deaf78dd4df73126bdc118c576eb165345
Jiwoong CHOI <jwchoi179@soongsil.ac.kr>
2024-09-19 15:59:01 +0900

    [#83] BaseView & BaseViewController 코드 내 문서 작성 및 추상화 강화

--- EATSSU_MVC/EATSSU_MVC/Sources/Utility/Base/BaseViewController.swift
+++ EATSSU_MVC/EATSSU_MVC/Sources/Utility/Base/BaseViewController.swift
@@ -5,11 +5,23 @@
 //  Created by 박윤빈 on 2023/03/15.
 //
 
+// TODO: BaseViewController 코드 Utility 모듈로 모듈화 간 재정비
+
 import UIKit
 
 import Moya
 import SnapKit
 
+/// EATSSU 앱에서 Controller로 사용하는 BaseViewController 클래스입니다.
+///
+/// # 소속 메소드
+/// - configureUI
+/// - setLayout
+/// - setButtonEvent
+/// - setCustomnavigationBar
+///
+/// - Important: configureUI와 setLayout 메소드는 필수로 오버라이딩 해야 합니다.
+/// 오버라이딩 하지 않으면 런타임 에러가 발생합니다.
 class BaseViewController: UIViewController {
     
     // MARK: - Properties
@@ -18,7 +30,7 @@ class BaseViewController: UIViewController {
       return type(of: self).description().components(separatedBy: ".").last ?? ""
     }()
     
-    // MARK: - Initializing
+    // MARK: - Initialize
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nil, bundle: nil)
@@ -29,6 +41,7 @@ class BaseViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+	// TODO: deinit 메소드의 목적이 무엇인지 알아보기
     deinit {
         print("DEINIT: \(className)")
     }
@@ -36,51 +49,82 @@ class BaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setCustomNavigationBar()
         configureUI()
         setLayout()
         setButtonEvent()
+		setCustomNavigationBar()
         view.backgroundColor = .systemBackground
-
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         if !NetworkMonitor.shared.isConnected {
-            print("네트워크오류")
+            print("네트워크 오류")
             self.showAlertController(title: "오류", message: "네트워크를 확인해주세요", style: .destructive)
-
         }
     }
     
-    //MARK: - Functions
+	// MARK: - Functions
+  
+	/// UIViewController에서 사용 중인 UIView를 연결합니다.
+	///
+	/// 메소드를 오버라이딩해서 View로 사용할 UIView 클래스를 연결하세요.
+	///
+	/// # Example
+	///
+	/// ```swift
+	///	override func configureUI() {
+	///		view.addSubview(rootView)
+	///	}
+	/// ```
+	func configureUI() {
+		fatalError("configureUI() must be overridden")
+	}
+	
+	/// 연결된 UIView 클래스의 레이아웃을 UIViewController의 View 프로퍼티를 기준으로 레이아웃을 조정합니다.
+	///
+	/// 메소드를 오버라이딩해서 연결된 UIView 클래스의 레이아웃을 조정하세요.
+	///
+	/// # Example
+	///
+	///	```swift
+	///	override func setLayout() {
+	///		rootView.snp.makeConstraints { make in
+	///			make.edges.equalToSuperView()
+	///		}
+	///	}
+	///	```
+	func setLayout() {
+		fatalError("setLayout() must be overridden")
+	}
     
-    func configureUI() {
-        //override Point
-
-    }
+	/// UIViewController에서 버튼이 있다면 버튼 액션을 연결해주세요.
+	///
+	/// 버튼이 있다면 오버라이딩해서 버튼 액션을 연결해주세요.
+	/// 없는 UIViewController도 있기 때문에 오버라이딩을 강조하지 않습니다.
+	func setButtonEvent() {}
     
-    func setLayout() {
-        //override Point
-    }
-    
-    func setButtonEvent() {
-        //override Point
-    }
-    
-    func setCustomNavigationBar() {
+	/// EATSSU 앱에서 사용하고 있는 네비게이션 바의 속성을 정의합니다.
+	///
+	/// # 네비게이션 타이틀 속성
+	///	- `gray700`으로 타이틀 색상을 설정합니다.
+	///	- `Pretendard Bold 16`으로 폰트를 설정합니다.
+	///
+	/// # 네비게이션 백버튼 속성
+	/// - `gray500`으로 백버튼 색상을 설정합니다.
+	func setCustomNavigationBar() {
+  
+		// 네비게이션 바 타이틀 속성
+		navigationController?.navigationBar.titleTextAttributes = [
+			.foregroundColor: EATSSUAsset.Color.GrayScale.gray700.color,
+			NSAttributedString.Key.font: EATSSUFontFamily.Pretendard.bold.font(size: 16)
+		]
       
-      // 네비게이션 바 타이틀 속성
-        navigationController?.navigationBar.titleTextAttributes = [
-          .foregroundColor: EATSSUAsset.Color.GrayScale.gray700.color,
-          NSAttributedString.Key.font: EATSSUFontFamily.Pretendard.bold.font(size: 16)
-        ]
-      
-      // 네비게이션 바 백버튼 속성
-        let backButton = UIBarButtonItem()
-      backButton.tintColor = EATSSUAsset.Color.GrayScale.gray500.color
-        navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
+		// 네비게이션 바 백버튼 속성
+		let backButton = UIBarButtonItem()
+		backButton.tintColor = EATSSUAsset.Color.GrayScale.gray500.color
+		navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
     }
 }
 
