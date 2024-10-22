2c22658252491f5e0f29bb0cd9ed3a0bf0162499
CJiu01 <jiwoo020801@gmail.com>
2024-10-22 15:07:47 +0900

    [#145] MyPageView -> underLineView 추가

--- EATSSU_MVC/EATSSU_MVC/Sources/Presentation/MyPage/View/MyPageView/MyPageView.swift
+++ EATSSU_MVC/EATSSU_MVC/Sources/Presentation/MyPage/View/MyPageView/MyPageView.swift
@@ -122,7 +122,8 @@ final class MyPageView: BaseUIView {
           myPageTableView,
           appVersionStringLabel,
           appVersionLabel,
-          userWithdrawButton
+          userWithdrawButton,
+          underLineView
       )
 	}
 	
