e676a26cf34a996302bb82f02d263a94849a8595
Jiwoong CHOI <jwchoi179@soongsil.ac.kr>
2024-11-06 10:48:35 +0900

    [#71] EATSSUComponent 모듈 EATSSUDesign으로 모듈 이름 변경

--- Workspace.swift
+++ Workspace.swift
@@ -5,5 +5,5 @@ let workspace = Workspace(name: "EATSSU_WORKSPACE", projects: [
 //  MVVM 프로젝트를 설계하기 시작하면 재활성화 하면 됩니다.
 //  현재 번들ID가 같아서 시뮬레이터에서 혼선이 발생하는 경우가 너무 많아서 시행하는 조치입니다.
 //  "EATSSU_MVVM",
-  "EATSSUComponents"
+  "EATSSUDesign"
 ])
