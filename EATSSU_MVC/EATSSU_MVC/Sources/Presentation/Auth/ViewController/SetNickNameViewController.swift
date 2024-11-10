a93966d3aa35263c4479b90a9dfbdb05f810fa5c
Jiwoong CHOI <jwchoi179@soongsil.ac.kr>
2024-11-10 16:41:45 +0900

    [#167] SetNickNameViewController에서 비즈니스 로직, 모델 객체로 분리

--- EATSSU_MVC/EATSSU_MVC/Sources/Presentation/Auth/ViewController/SetNickNameViewController.swift
+++ EATSSU_MVC/EATSSU_MVC/Sources/Presentation/Auth/ViewController/SetNickNameViewController.swift
@@ -60,33 +60,77 @@ final class SetNickNameViewController: BaseViewController {
     
 	@objc
 	func tappedCompleteNickNameButton() {
-		model.setUserNickname(nickname: .setNickNameView.inputNickNameTextField.text ?? "")
+		firstly {
+			model.setUserNickname(nickname: setNickNameView.inputNickNameTextField.text ?? "")
+		}.done {
+			// FIXME: 문자열 리소스 관리 파일로 전환
+			self.showAlertController(title: "완료", message: "닉네임 설정이 완료되었습니다.", style: .cancel) {
+				if let myPageViewController = self.navigationController?.viewControllers.first(where: { $0 is MyPageViewController }) {
+					self.navigationController?.popToViewController(myPageViewController, animated: true)
+				} else {
+					let homeViewController = HomeViewController()
+					if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+					   let keyWindow = windowScene.windows.first(where: { $0.isKeyWindow })
+					{
+						keyWindow.replaceRootViewController(UINavigationController(rootViewController: homeViewController))
+					}
+				}
+			}
+		}.catch { err in
+			print(err.localizedDescription)
+		}
 	}
     
 	@objc
 	private func tappedCheckButton() {
-		model.checkNickname(nickname: setNickNameView.inputNickNameTextField.text ?? "")
+		firstly {
+			model.checkNickname(nickname: setNickNameView.inputNickNameTextField.text ?? "")
+		}.done { isSuccess in
+			if isSuccess {
+				// FIXME: 문자열 리소스 관리파일로 전환
+				self.view.showToast(message: "사용 가능한 닉네임이에요")
+				self.setNickNameView.completeSettingNickNameButton.isEnabled = isSuccess
+				self.setNickNameView.nicknameValidationMessageLabel.text = NicknameTextFieldResultType.nicknameTextFieldValid.hintMessage
+				self.setNickNameView.nicknameValidationMessageLabel.textColor = NicknameTextFieldResultType.nicknameTextFieldValid.textColor
+			} else {
+				// FIXME: 문자열 리소스 관리파일로 전환
+				self.view.showToast(message: "이미 사용 중인 닉네임이에요")
+				self.setNickNameView.completeSettingNickNameButton.isEnabled = isSuccess
+				self.setNickNameView.nicknameValidationMessageLabel.text = NicknameTextFieldResultType.nicknameTextFieldDuplicated.hintMessage
+				self.setNickNameView.nicknameValidationMessageLabel.textColor = NicknameTextFieldResultType.nicknameTextFieldDuplicated.textColor
+			}
+		}.catch { err in
+			print(err.localizedDescription)
+		}
 	}
     
-	// MARK: - keyboard 감지
+	// MARK: - 키보드 감지
     
 	func addKeyboardNotifications() {
-		NotificationCenter.default.addObserver(self,
-		                                       selector: #selector(keyboardWillShow(_:)),
-		                                       name: UIResponder.keyboardWillShowNotification,
-		                                       object: nil)
-		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)),
-		                                       name: UIResponder.keyboardWillHideNotification,
-		                                       object: nil)
+		NotificationCenter
+			.default
+			.addObserver(self,
+			             selector: #selector(keyboardWillShow(_:)),
+			             name: UIResponder.keyboardWillShowNotification,
+			             object: nil)
+		NotificationCenter
+			.default
+			.addObserver(self, selector: #selector(keyboardWillHide(_:)),
+			             name: UIResponder.keyboardWillHideNotification,
+			             object: nil)
 	}
 
 	func removeKeyboardNotifications() {
-		NotificationCenter.default.removeObserver(self,
-		                                          name: UIResponder.keyboardWillShowNotification,
-		                                          object: nil)
-		NotificationCenter.default.removeObserver(self,
-		                                          name: UIResponder.keyboardWillHideNotification,
-		                                          object: nil)
+		NotificationCenter
+			.default
+			.removeObserver(self,
+			                name: UIResponder.keyboardWillShowNotification,
+			                object: nil)
+		NotificationCenter
+			.default
+			.removeObserver(self,
+			                name: UIResponder.keyboardWillHideNotification,
+			                object: nil)
 	}
 
 	@objc
@@ -100,7 +144,8 @@ final class SetNickNameViewController: BaseViewController {
 		}
 	}
 
-	@objc func keyboardWillHide(_ notification: Notification) {
+	@objc
+	func keyboardWillHide(_ notification: Notification) {
 		// FIXME: 의미를 알 수 없는 경고가 발생
 		if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
 			setNickNameView.completeSettingNickNameButton.frame.origin.y += currentKeyboardHeight
