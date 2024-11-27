252a5c5b9e7d82a5e7768628caa5feb344994ddc
CJiu01 <jiwoo020801@gmail.com>
2024-11-27 18:19:32 +0900

    [#90] LoginPromptVC 코드 구조 통일 작업

--- EATSSU_MVC/EATSSU_MVC/Sources/Presentation/Review/ViewController/LoginPromptViewController.swift
+++ EATSSU_MVC/EATSSU_MVC/Sources/Presentation/Review/ViewController/LoginPromptViewController.swift
@@ -11,33 +11,36 @@ final class LoginPromptViewController: BaseViewController {
     
     // MARK: - UI Components
     
-    private let loginAlertLabel: UILabel = {
-        let label = UILabel()
-        label.text = "3초만에 로그인하고\n리뷰를 달아보세요!"
-        label.textAlignment = .left
-        label.numberOfLines = 0
-        label.font = EATSSUFontFamily.Pretendard.bold.font(size: 18)
-        return label
-    }()
+    private let loginAlertLabel = UILabel()
+    private let appleLoginButton = UIButton()
+    private let kakaoLoginButton = UIButton()
     
-    private let appleLoginButton: UIButton = {
-        let button = UIButton()
-        button.setImage(EATSSUAsset.Images.Version2.appleLoginButton.image, for: .normal)
-        return button
-    }()
-    
-    private let kakaoLoginButton: UIButton = {
-        let button = UIButton()
-        button.setImage(EATSSUAsset.Images.Version2.kakaoLoginButton.image, for: .normal)
-        return button
-    }()
+    // MARK: - Life Cycles
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        view.backgroundColor = .yellow
+        setViewProperties()
     }
     
+    // MARK: - Functions
+    
+    private func setViewProperties() {
+        loginAlertLabel.do {
+            $0.text = TextLiteral.SignIn.loginPrompt
+            $0.textAlignment = .left
+            $0.numberOfLines = 0
+            $0.font = EATSSUFontFamily.Pretendard.bold.font(size: 18)
+        }
+        
+        appleLoginButton.do {
+            $0.setImage(EATSSUAsset.Images.Version2.appleLoginButton.image, for: .normal)
+        }
+        
+        kakaoLoginButton.do {
+            $0.setImage(EATSSUAsset.Images.Version2.kakaoLoginButton.image, for: .normal)
+        }
+    }
     
     override func configureUI() {
         view.addSubviews(
@@ -51,12 +54,14 @@ final class LoginPromptViewController: BaseViewController {
         loginAlertLabel.snp.makeConstraints {
             $0.top.leading.equalTo(view.safeAreaLayoutGuide).offset(30.adjusted)
         }
+        
         appleLoginButton.snp.makeConstraints {
-//            $0.top.equalTo(loginAlertLabel.snp.bottom).offset(44.adjusted)
+            // $0.top.equalTo(loginAlertLabel.snp.bottom).offset(44.adjusted)
             $0.bottom.equalTo(kakaoLoginButton.snp.top).offset(8)
             $0.centerX.equalToSuperview()
             
         }
+        
         kakaoLoginButton.snp.makeConstraints {
             $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(30)
             $0.centerX.equalToSuperview()
