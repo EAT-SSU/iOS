4ee0f6a13d3233e146785c3d95125e7a4f015b51
CJiu01 <jiwoo020801@gmail.com>
2024-11-28 15:58:03 +0900

    [#90] 코드 정리

--- EATSSU_MVC/EATSSU_MVC/Sources/Presentation/Home/ViewController/HomeViewController.swift
+++ EATSSU_MVC/EATSSU_MVC/Sources/Presentation/Home/ViewController/HomeViewController.swift
@@ -126,15 +126,10 @@ final class HomeViewController: BaseViewController {
                 }
                 present(loginPromptVC, animated: true, completion: nil)
             }
-            
-            
-            
-            
-            
+
 //            showAlertControllerWithCancel(title: "로그인이 필요한 서비스입니다", message: "로그인 하시겠습니까?", confirmStyle: .default) {
 //                self.changeIntoLoginViewController()
 //            }
-            
             
         }
     }
