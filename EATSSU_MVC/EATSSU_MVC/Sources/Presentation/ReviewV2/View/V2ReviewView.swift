e7cf253c17d8c9e5bb8a9cdfb7de1ae3d74b758c
CJiu01 <jiwoo020801@gmail.com>
2024-12-14 19:20:55 +0900

    [#195] ReviewV2 foldering

---
+++ EATSSU_MVC/EATSSU_MVC/Sources/Presentation/ReviewV2/View/V2ReviewView.swift
@@ -0,0 +1,8 @@
+//
+//  V2ReviewView.swift
+//  EATSSU
+//
+//  Created by 최지우 on 12/14/24.
+//
+
+import Foundation
---
+++ EATSSU_MVC/EATSSU_MVC/Sources/Presentation/ReviewV2/ViewController/V2ReviewViewController.swift
@@ -0,0 +1,12 @@
+//
+//  V2ReviewViewController.swift
+//  EATSSU
+//
+//  Created by 최지우 on 12/14/24.
+//
+
+import UIKit
+
+final class V2ReviewViewController: BaseViewController {
+
+}
