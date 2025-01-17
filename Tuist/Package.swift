b6b3ebf6b0ea61cfadbb5f6837ca90a9fe2f376a
Jiwoong <jwchoi179@soongsil.ac.kr>
2025-01-17 12:14:49 +0900

    [#12] 모든 외부 라이브러리를 정적 프레임워크로 전환

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -1,17 +1,37 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 import PackageDescription
 
 #if TUIST
     import ProjectDescription
 
+    // Tuist가 #if TUIST 영역을 해석할 때,
+    // 각 SPM 라이브러리를 동적 프레임워크(.framework)로 빌드하도록 설정
     let packageSettings = PackageSettings(
-        // Customize the product types for specific package product
-        // Default is .staticFramework
-        // productTypes: ["Alamofire": .framework,]
         productTypes: [
+            // 예시: SnapKit/Tabman/Moya/Then/FSCalendar/Kakao/Kingfisher/Firebase/Realm/RxSwift ...
             "SnapKit": .framework,
-            "Kingfisher": .framework,
+            "Tabman": .framework,
             "Moya": .framework,
+            "Then": .framework,
+            "FSCalendar": .framework,
+            "Kingfisher": .framework,
+
+            // Kakao iOS SDK
+            // 실제 사용 모듈(예: KakaoSDKAuth, KakaoSDKUser 등)을 정확히 써야 함
+            "KakaoSDKCommon": .framework,
+            "KakaoSDKAuth": .framework,
+            "KakaoSDKUser": .framework,
+
+            // Firebase 계열 (예: FirebaseAnalytics, FirebaseCore, FirebaseAuth 등)
+            "Firebase": .framework,
+            "GoogleAppMeasurement": .framework,
+
+            // realm-swift
+            "Realm": .framework,
+            "RealmSwift": .framework,
+
+            // RxSwift
+            "RxSwift": .framework,
         ]
     )
 #endif
@@ -19,9 +39,6 @@ import PackageDescription
 let package = Package(
     name: "EATSSU_WORKSPACE",
     dependencies: [
-        // Add your own dependencies here:
-        // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
-        // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
         .package(url: "https://github.com/SnapKit/SnapKit", from: "5.7.1"),
         .package(url: "https://github.com/uias/Tabman", from: "3.2.0"),
         .package(url: "https://github.com/Moya/Moya", from: "15.0.0"),
