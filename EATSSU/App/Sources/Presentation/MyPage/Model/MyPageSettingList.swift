f040ce84da4c7f7827ddce25cbc952a019ae565a
Jiwoong <jwchoi179@soongsil.ac.kr>
2025-01-31 16:29:43 +0900

    [#234] MyPageViewController 리팩터링
    - EATSSUKit에 AlertController 추출
    - Xcode markup 작성

---
+++ EATSSU/App/Sources/Presentation/MyPage/Model/MyPageSettingList.swift
@@ -0,0 +1,37 @@
+//
+//  MyPageSettingList.swift
+//  EATSSU
+//
+//  Created by Jiwoong CHOI on 9/19/24.
+//
+
+import Foundation
+
+struct MyPageSettingList {
+    let title: String
+}
+
+extension MyPageSettingList {
+    static let myPageTableLabelList = [
+        // "푸시 알림 설정"
+        MyPageSettingList(title: ESTextLiteral.MyPage.pushNotificationSetting),
+
+        // "내가 쓴 리뷰"
+        MyPageSettingList(title: ESTextLiteral.MyPage.myReview),
+
+        // "문의하기"
+        MyPageSettingList(title: ESTextLiteral.MyPage.inquiry),
+
+        // "서비스 이용약관"
+        MyPageSettingList(title: ESTextLiteral.MyPage.termsOfUse),
+
+        // "개인정보 이용약관"
+        MyPageSettingList(title: ESTextLiteral.MyPage.privacyTermsOfUse),
+
+        // "만든 사람들"
+        MyPageSettingList(title: ESTextLiteral.MyPage.creators),
+
+        // "로그아웃"
+        MyPageSettingList(title: ESTextLiteral.MyPage.logout),
+    ]
+}
