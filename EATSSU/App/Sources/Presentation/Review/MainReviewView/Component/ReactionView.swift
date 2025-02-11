c005daff059c3cee7a36e13d68e6b66cbba9cc55
CJiu01 <jiwoo020801@gmail.com>
2025-02-11 17:49:48 +0900

    [#198] 리뷰v2 파일 생성 및 사용하지 않는 파일 제거

---
+++ EATSSU/App/Sources/Presentation/Review/MainReviewView/Component/ReactionView.swift
@@ -0,0 +1,8 @@
+//
+//  ReactionView.swift
+//  EATSSU
+//
+//  Created by 최지우 on 2/11/25.
+//
+
+import Foundation
---
+++ EATSSU/App/Sources/Presentation/Review/MainReviewView/WritingReview/WritingReviewModalView.swift
@@ -0,0 +1,15 @@
+//
+//  WritingReviewModalView.swift
+//  SnapKit
+//
+//  Created by 최지우 on 2/2/25.
+//
+
+import UIKit
+
+import EATSSUDesign
+
+final class WritingReviewModalView: BaseUIView {
+    
+    
+}
