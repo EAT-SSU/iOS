79bb96eda0d034f379164dcff46bb8889b57a252
CJiu01 <jiwoo020801@gmail.com>
2025-02-11 17:54:31 +0900

    [#198] ReactionView 제작

--- EATSSU/App/Sources/Presentation/Review/MainReviewView/Component/ReactionView.swift
+++ EATSSU/App/Sources/Presentation/Review/MainReviewView/Component/ReactionView.swift
@@ -5,4 +5,47 @@
 //  Created by 최지우 on 2/11/25.
 //
 
-import Foundation
+import UIKit
+
+import EATSSUDesign
+import SnapKit
+
+final class ReactionView: BaseUIView {
+    
+    // MARK: - UI Components
+    
+    private let unlikeImageView: UIImageView = {
+        let imageView = UIImageView(image: EATSSUDesignAsset.Images.like.image)
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let likeCountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "3"
+        label.font = EATSSUDesignFontFamily.Pretendard.medium.font(size: 12)
+        return label
+    }()
+    
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [unlikeImageView, likeCountLabel])
+        stackView.axis = .horizontal
+        stackView.spacing = 3
+        return stackView
+    }()
+    
+    // MARK: - Functions
+    
+    override func configureUI() {
+        addSubview(stackView)
+    }
+    
+    override func setLayout() {
+        stackView.snp.makeConstraints { make in
+            make.height.equalTo(20)
+        }
+        unlikeImageView.snp.makeConstraints { make in
+            make.width.height.equalTo(12)
+        }
+    }
+}
