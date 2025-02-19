aac34e64ff6a3b1bb78237263465932b8f6615b7
CJiu01 <jiwoo020801@gmail.com>
2025-02-19 17:53:44 +0900

    [#198] RateNumberView -> StarRatingView 네이밍 변경

--- EATSSU/App/Sources/Presentation/Review/View/SeeReview/StarRatingView.swift
+++ EATSSU/App/Sources/Presentation/Review/View/SeeReview/StarRatingView.swift
@@ -8,18 +8,28 @@
 import UIKit
 
 import EATSSUDesign
-
 import SnapKit
-import Then
 
-final class RateNumberView: BaseUIView {
+final class StarRatingView: BaseUIView {
+    
     // MARK: - UI Components
 
-    let starImageView = UIImageView()
-    lazy var rateNumberLabel = UILabel()
+    private let starImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = EATSSUDesignAsset.Images.icStarYellow.image
+        return imageView
+    }()
+    
+    private lazy var rateNumberLabel: UILabel = {
+        let label = UILabel()
+        label.text = "5"
+        label.font = EATSSUDesignFontFamily.Pretendard.medium.font(size: 40)
+        return label
+    }()
+    
     private lazy var rateNumberStackView = UIStackView(arrangedSubviews: [starImageView,
                                                                           rateNumberLabel])
-
+    
     // MARK: - init
 
     override init(frame: CGRect) {
@@ -30,40 +40,35 @@ final class RateNumberView: BaseUIView {
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-    }
-
+    
     // MARK: - Functions
 
     override func configureUI() {
         addSubviews(rateNumberStackView)
-        starImageView.do {
-            $0.image = EATSSUDesignAsset.Images.icStarYellow.image
-        }
 
         rateNumberLabel.do {
             $0.text = "5"
-            $0.font = .body2
-            $0.textColor = EATSSUDesignAsset.Color.Main.primary.color
+            $0.font = EATSSUDesignFontFamily.Pretendard.medium.font(size: 40)
         }
 
         rateNumberStackView.do {
             $0.axis = .horizontal
-            $0.spacing = 3
-            $0.alignment = .top
+            $0.spacing = 8
         }
     }
 
     override func setLayout() {
         starImageView.snp.makeConstraints {
-            $0.height.equalTo(12.adjusted)
-            $0.width.equalTo(12.adjusted)
+            $0.height.equalTo(24.adjusted)
+            $0.width.equalTo(24.adjusted)
         }
 
         rateNumberStackView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
+    }
+    
+    public func updateStarRating(_ rate: Double) {
+        rateNumberLabel.text = String(format: "%.1f", rate)
     }
 }
