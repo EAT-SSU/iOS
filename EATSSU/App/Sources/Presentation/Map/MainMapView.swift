470e95b64062481c8ecc0e5f129c323ca0abeefa
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2025-06-25 00:12:26 +0900

    [#234] map UI 1차 구현

---
+++ EATSSU/App/Sources/Presentation/Map/MainMapView.swift
@@ -0,0 +1,118 @@
+//
+//  MainMapView.swift
+//  EATSSU-DEV
+//
+//  Created by 황상환 on 6/24/25.
+//
+
+import UIKit
+import NMapsMap
+import SnapKit
+import EATSSUDesign
+
+final class MainMapView: UIView {
+
+    let mapView = NMFNaverMapView()
+    let toggleBackgroundView = UIView()
+    let wholeButton = UIButton(type: .system)
+    let myOnlyButton = UIButton(type: .system)
+    let heartButton = UIButton(type: .system)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+        setLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+    private func setup() {
+        backgroundColor = .white
+
+        mapView.showZoomControls = false
+        addSubview(mapView)
+
+        toggleBackgroundView.layer.cornerRadius = 20
+        toggleBackgroundView.layer.borderWidth = 1
+        toggleBackgroundView.layer.borderColor = EATSSUDesignAsset.Color.GrayScale.gray300.color.cgColor
+        toggleBackgroundView.backgroundColor = .white
+        addSubview(toggleBackgroundView)
+
+        [wholeButton, myOnlyButton].forEach {
+            $0.titleLabel?.font = EATSSUDesignFontFamily.Pretendard.regular.font(size: 14)
+            $0.layer.cornerRadius = 14
+            $0.clipsToBounds = true
+            $0.backgroundColor = .clear
+            $0.setTitleColor(.label, for: .normal)
+        }
+
+        wholeButton.setTitle("전체", for: .normal)
+        myOnlyButton.setTitle("내 제휴", for: .normal)
+        selectWhole(true) // 초기 선택 상태
+        toggleBackgroundView.addSubview(wholeButton)
+        toggleBackgroundView.addSubview(myOnlyButton)
+
+        let heartImage = UIImage(systemName: "heart")?
+            .withConfiguration(UIImage.SymbolConfiguration(pointSize: 13, weight: .regular))
+        heartButton.setImage(heartImage, for: .normal)
+        heartButton.tintColor = EATSSUDesignAsset.Color.Red.error.color
+        heartButton.backgroundColor = .white
+        heartButton.layer.cornerRadius = 20
+        heartButton.layer.shadowColor = UIColor.black.cgColor
+        heartButton.layer.shadowOpacity = 0.1
+        heartButton.layer.shadowRadius = 4
+        heartButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+        addSubview(heartButton)
+    }
+
+    private func setLayout() {
+        mapView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        toggleBackgroundView.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide).offset(12)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(180)
+            $0.height.equalTo(40)
+        }
+
+        wholeButton.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(4)
+            $0.leading.equalToSuperview().inset(4)
+            $0.width.equalTo(80)
+        }
+
+        myOnlyButton.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(4)
+            $0.trailing.equalToSuperview().inset(4)
+            $0.width.equalTo(80)
+        }
+
+        heartButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(16)
+            $0.top.equalTo(safeAreaLayoutGuide).offset(12)
+            $0.size.equalTo(40)
+        }
+    }
+
+
+    func selectWhole(_ isSelected: Bool) {
+        if isSelected {
+            wholeButton.backgroundColor = EATSSUDesignAsset.Color.Main.primary.color
+            wholeButton.setTitleColor(.white, for: .normal)
+
+            myOnlyButton.backgroundColor = .clear
+            myOnlyButton.setTitleColor(.label, for: .normal)
+        } else {
+            wholeButton.backgroundColor = .clear
+            wholeButton.setTitleColor(.label, for: .normal)
+
+            myOnlyButton.backgroundColor = EATSSUDesignAsset.Color.Main.primary.color
+            myOnlyButton.setTitleColor(.white, for: .normal)
+        }
+    }
+
+}
---
+++ EATSSU/App/Sources/Presentation/Map/MainMapViewController.swift
@@ -0,0 +1,58 @@
+//
+//  MainMapViewController.swift
+//  EATSSU-DEV
+//
+//  Created by 황상환 on 6/24/25.
+//
+
+import UIKit
+import NMapsMap
+
+import EATSSUDesign
+
+final class MainMapViewController: UIViewController {
+
+    private let mainView = MainMapView()
+
+    override func loadView() {
+        self.view = mainView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "제휴 지도"
+
+        // 배경색 채우기
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        navBarAppearance.backgroundColor = .white // 원하는 색상
+        navBarAppearance.titleTextAttributes = [
+            .foregroundColor: UIColor.black,
+            .font: EATSSUDesignFontFamily.Pretendard.bold.font(size: 16)
+        ]
+
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+        navigationController?.navigationBar.compactAppearance = navBarAppearance
+
+        mainView.wholeButton.addTarget(self, action: #selector(didTapWhole), for: .touchUpInside)
+        mainView.myOnlyButton.addTarget(self, action: #selector(didTapMyOnly), for: .touchUpInside)
+        mainView.heartButton.addTarget(self, action: #selector(didTapHeart), for: .touchUpInside)
+        
+    }
+
+    @objc private func didTapWhole() {
+        mainView.selectWhole(true)
+        print("전체 보기")
+    }
+
+    @objc private func didTapMyOnly() {
+        mainView.selectWhole(false)
+        print("내 제휴 보기")
+    }
+
+    @objc private func didTapHeart() {
+        print("하트 버튼 클릭됨")
+    }
+}
