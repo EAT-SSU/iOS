f8015b74926a6e39767c9e1fd42a8abc4b770491
Funital <gksrmawns0925@naver.com>
2025-08-08 00:38:55 +0900

    [#304] url 연결 코드의 중복 코드 수정
    #304

--- EATSSU/App/Sources/Presentation/MyPage/ViewController/CreatorViewController.swift
+++ EATSSU/App/Sources/Presentation/MyPage/ViewController/CreatorViewController.swift
@@ -15,6 +15,10 @@ import EATSSUDesign
 
 class CreatorViewController: BaseViewController {
     // MARK: - Properties
+    private enum URLConstants {
+        static let instagram = "https://www.instagram.com/eatssu.official/"
+        static let landingPage = "https://eat-ssu.notion.site/eat-ssu-landing"
+    }
 
     // View Properties
     private let creatorsView = CreatorsView()
@@ -69,29 +73,29 @@ class CreatorViewController: BaseViewController {
         gradient.endPoint = CGPoint(x: 0.5, y: 1.0)
         view.layer.insertSublayer(gradient, at: 0)
     }
-    
+
     private func setUpAction() {
         /// @eatssu_official 인스타그램 연결
         creatorsView.instagramButton.addTarget(self, action: #selector(openInstagram), for: .touchUpInside)
-        
+
         /// eatssu 랜딩페이지 연결
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(nextCreatorsImageTapped))
         creatorsView.nextCreatorsImageView.addGestureRecognizer(tapGesture)
     }
-    
+
+    private func open(urlString: String) {
+        guard let url = URL(string: urlString),
+              UIApplication.shared.canOpenURL(url) else { return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+
     /// @eatssu_official 인스타그램 연결 동작
     @objc private func openInstagram() {
-        if let url = URL(string: "https://www.instagram.com/eatssu.official/"),
-           UIApplication.shared.canOpenURL(url) {
-            UIApplication.shared.open(url, options: [:], completionHandler: nil)
-        }
+        open(urlString: URLConstants.instagram)
     }
-    
+
     /// eatssu 랜딩페이지 연결 동작
     @objc private func nextCreatorsImageTapped() {
-        if let url = URL(string: "https://eat-ssu.notion.site/eat-ssu-landing"),
-           UIApplication.shared.canOpenURL(url) {
-            UIApplication.shared.open(url, options: [:], completionHandler: nil)
-        }
+        open(urlString: URLConstants.landingPage)
     }
 }
