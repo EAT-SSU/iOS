ba76654313e3cf15074e550282f184df6f492275
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2025-10-22 16:31:00 +0900

    [#343] ReviewEmptyViewCell UI 업데이트

--- EATSSU/App/Sources/Presentation/Review/View/SeeReview/ReviewEmptyViewCell.swift
+++ EATSSU/App/Sources/Presentation/Review/View/SeeReview/ReviewEmptyViewCell.swift
@@ -9,6 +9,8 @@ import UIKit
 
 import SnapKit
 
+import EATSSUDesign
+
 final class ReviewEmptyViewCell: UITableViewCell {
     // MARK: - Properties
 
@@ -16,17 +18,44 @@ final class ReviewEmptyViewCell: UITableViewCell {
 
     // MARK: - UI Components
 
-    private lazy var noReviewImageView: UIImageView = {
+    private lazy var reviewIconImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.image = ImageLiteral.noReview
+        imageView.image = EATSSUDesignAsset.Images.revviewIcon.image
+        imageView.contentMode = .scaleAspectFit
         return imageView
+    }()
+    
+    private lazy var mainLabel: UILabel = {
+        let label = UILabel()
+        label.text = "아직 작성된 리뷰가 없어요!"
+        label.font = EATSSUDesignFontFamily.Pretendard.medium.font(size: 16)
+        label.textColor = EATSSUDesignAsset.Color.GrayScale.gray600.color
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var subLabel: UILabel = {
+        let label = UILabel()
+        label.text = "메뉴에 가장 먼저 리뷰를 남겨주세요"
+        label.font = EATSSUDesignFontFamily.Pretendard.medium.font(size: 12)
+        label.textColor = EATSSUDesignAsset.Color.GrayScale.gray600.color
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var contentStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [reviewIconImageView, mainLabel, subLabel])
+        stackView.axis = .vertical
+        stackView.spacing = 12
+        stackView.alignment = .center
+        return stackView
     }()
 
     // MARK: - Functions
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.addSubview(noReviewImageView)
+        contentView.addSubview(contentStackView)
         setLayout()
     }
 
@@ -36,16 +65,22 @@ final class ReviewEmptyViewCell: UITableViewCell {
     }
 
     func setLayout() {
-        noReviewImageView.snp.makeConstraints {
+        reviewIconImageView.snp.makeConstraints {
+            $0.width.height.equalTo(48)
+        }
+        
+        contentStackView.snp.makeConstraints {
             $0.center.equalToSuperview()
         }
     }
 
     func configure(isTokenExist: Bool) {
         if isTokenExist {
-            noReviewImageView.image = ImageLiteral.noReview
+            mainLabel.text = "아직 작성된 리뷰가 없어요!"
+            subLabel.text = "메뉴에 가장 먼저 리뷰를 남겨주세요"
         } else {
-            noReviewImageView.image = ImageLiteral.pleaseLogin
+            mainLabel.text = "로그인이 필요합니다"
+            subLabel.text = "로그인 후 리뷰를 확인하세요"
         }
     }
 }
