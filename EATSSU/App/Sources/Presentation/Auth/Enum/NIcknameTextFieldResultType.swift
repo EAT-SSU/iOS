4d9bcb960a22457375d9391a92722cd60d26bab8
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2025-10-24 23:12:36 +0900

    [#327] 닉네임 타입 및 케이스 추가

--- EATSSU/App/Sources/Presentation/Auth/Enum/NIcknameTextFieldResultType.swift
+++ EATSSU/App/Sources/Presentation/Auth/Enum/NIcknameTextFieldResultType.swift
@@ -15,7 +15,12 @@ enum NicknameTextFieldResultType {
     case nicknameTextFieldDuplicated
     case nicknameTextFieldDoubleCheck
     case nicknameTextFieldValid
-
+    case invalidLength
+    case invalidStartOrEnd          
+    case consecutiveSpecialChars
+    case onlyNumbers
+    case invalidCharacters
+    
     var hintMessage: String {
         switch self {
         case .textFieldEmpty:
@@ -28,12 +33,23 @@ enum NicknameTextFieldResultType {
             "이미 사용 중인 닉네임이에요"
         case .nicknameTextFieldValid:
             "사용가능한 닉네임이에요"
+        case .invalidLength:
+            "1~16자로 입력해주세요"
+        case .invalidStartOrEnd:
+            "한글, 영문, 숫자로 시작하고 끝나야 해요"
+        case .consecutiveSpecialChars:
+            "특수문자(-, _, 공백)는 연속으로 사용할 수 없어요"
+        case .onlyNumbers:
+            "숫자로만 구성할 수 없어요"
+        case .invalidCharacters:
+            "한글, 영문, 숫자, -, _, 공백만 사용 가능해요"
         }
     }
 
     var textColor: UIColor {
         switch self {
-        case .textFieldEmpty, .nicknameTextFieldOver, .nicknameTextFieldDuplicated, .nicknameTextFieldDoubleCheck:
+        case .textFieldEmpty, .nicknameTextFieldOver, .nicknameTextFieldDuplicated, .nicknameTextFieldDoubleCheck,
+             .invalidLength, .invalidStartOrEnd, .consecutiveSpecialChars, .onlyNumbers, .invalidCharacters:
             .primary
         case .nicknameTextFieldValid:
             .gray700
