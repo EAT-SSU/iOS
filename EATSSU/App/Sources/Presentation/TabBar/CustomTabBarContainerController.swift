21ca74a5697afd742ace06462bf752d2dcdc0645
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2025-11-19 17:54:08 +0900

    [#359] 사용하지 않는 커스텀 뷰 제거

--- EATSSU/App/Sources/Presentation/TabBar/CustomTabBarContainerController.swift
+++ EATSSU/App/Sources/Presentation/TabBar/CustomTabBarContainerController.swift
@@ -221,3 +221,12 @@ extension CustomTabBarContainerController: UITabBarControllerDelegate {
         return true
     }
 }
+
+extension UIImage {
+    func resized(to size: CGSize) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: size)
+        return renderer.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: size))
+        }
+    }
+}
