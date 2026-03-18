8abc18a9384e850a1d3cb58d9c677a24665d8364
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2026-03-18 23:09:04 +0900

    [#405] partnershipInfos가 비어있어도 click_partner_restaurant 이벤트 전송되도록 수정

--- EATSSU/App/Sources/Presentation/Map/ViewController/MainMapViewController+Marker.swift
+++ EATSSU/App/Sources/Presentation/Map/ViewController/MainMapViewController+Marker.swift
@@ -147,14 +147,11 @@ extension MainMapViewController {
     
     /// 제휴점 상세 바텀시트 표시
     func showPartnershipDetail(for partnership: PartnershipDTO) {
-        let partnerId = partnership.partnershipInfos.first?.id
-        if let partnerId = partnerId {
-            MapAnalyticsManager.shared.logClickPartnerRestaurant(
-                collegeId: currentCollegeId,
-                majorId: currentDepartmentId,
-                partnerId: partnerId
-            )
-        }
+        MapAnalyticsManager.shared.logClickPartnerRestaurant(
+            collegeId: currentCollegeId,
+            majorId: currentDepartmentId,
+            partnerId: partnership.partnershipInfos.first?.id ?? -1
+        )
 
         let detailVC = PartnershipDetailSheetViewController(
             storeName: partnership.storeName,
