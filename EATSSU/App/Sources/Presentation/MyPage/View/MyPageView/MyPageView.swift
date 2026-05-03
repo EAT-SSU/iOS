252ec8f330a11f6ee0224665514d67f6ed7d6348
jeongminji <parapin2274@gmail.com>
2026-05-03 22:31:45 +0900

    [#275] 마이페이지 UI 리뉴얼

--- EATSSU/App/Sources/Presentation/MyPage/View/MyPageView/MyPageView.swift
+++ EATSSU/App/Sources/Presentation/MyPage/View/MyPageView/MyPageView.swift
@@ -12,37 +12,73 @@ import SnapKit
 import EATSSUDesign
 
 final class MyPageView: BaseUIView {
-    // MARK: - UI Components
+    // MARK: - Properties
+    private static var appVersion: String {
+        guard let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String else {
+            return "-"
+        }
 
+        return version
+    }
+    
+    private var myPageTableViewHeight: CGFloat {
+        let rowTotalHeight = MyPageSectionData.sections.reduce(CGFloat(0)) { result, section in
+            let sectionRowHeight = section.items.reduce(CGFloat(0)) { rowResult, item in
+                return rowResult + MyPageTableMetric.rowHeight(for: item)
+            }
+
+            return result + sectionRowHeight
+        }
+
+        let sectionCount = MyPageSectionData.sections.count
+        let headerTotalHeight = CGFloat(sectionCount) * MyPageTableMetric.headerHeight
+
+        let footerCount = max(sectionCount - 1, 0)
+        let footerTotalHeight = CGFloat(footerCount) * MyPageTableMetric.footerHeight
+
+        return rowTotalHeight + headerTotalHeight + footerTotalHeight
+    }
+    
+    // MARK: - UI Components
+    
     /// MyPageView 전체 스크롤뷰
     private let scrollView = UIScrollView()
-
+    
     /// 스크롤뷰 안에 들어갈 콘텐츠 뷰
     private let contentView = UIView()
-
+    
     // 사용자 이미지
     var userImage: UIImageView = {
         let imageView = UIImageView()
         imageView.image = EATSSUDesignAsset.Images.profile.image
         return imageView
     }()
-
-    // 닉네임이 들어간 닉네임 변경 버튼
+    
+    // 유저 닉네임
     var userNicknameLabel: UILabel = {
         let label = UILabel()
         label.text = TextLiteral.MyPage.retry
         label.textColor = .gray700Basic
-        label.font = .header1
+        label.font = .subtitle2
         return label
     }()
-
+    
+    // 유저 소속
+    var userAffiliationLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .gray600
+        label.font = .body3
+        label.numberOfLines = 1
+        return label
+    }()
+    
     let myPageTableView: UITableView = {
         let tableView = UITableView()
         tableView.separatorStyle = .none
         tableView.isScrollEnabled = false
         return tableView
     }()
-
+    
     // "앱 버전" 레이블
     private let appVersionStringLabel: UILabel = {
         let label = UILabel()
@@ -51,16 +87,16 @@ final class MyPageView: BaseUIView {
         label.textColor = .gray400
         return label
     }()
-
+    
     // 현재 배포된 앱의 버전
     private let appVersionLabel: UILabel = {
         let label = UILabel()
-        label.text = MyPageRightItemData.version
+        label.text = appVersion
         label.font = .caption2
         label.textColor = .gray400
         return label
     }()
-
+    
     /// "탈퇴하기" 레이블과 탈퇴하기 아이콘
     let userWithdrawButton: UIButton = {
         let button = UIButton()
@@ -69,33 +105,35 @@ final class MyPageView: BaseUIView {
         button.setTitleColor(.gray400, for: .normal)
         button.titleLabel?.font = .caption2
         button.tintColor = .red
+        button.semanticContentAttribute = .forceRightToLeft
         return button
     }()
-
+    
     /// "탈퇴하기" 레이블 underline
     private let underLineView: UIView = {
         let view = UIView()
         view.backgroundColor = .gray400
         return view
     }()
-
+    
     // MARK: - Intializer
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-
+        
         registerTableViewCells()
     }
-
+    
     // MARK: - Functions
-
+    
     override func configureUI() {
         addSubview(scrollView)
         scrollView.addSubview(contentView)
-
+        
         contentView.addSubviews(
             userImage,
             userNicknameLabel,
+            userAffiliationLabel,
             myPageTableView,
             appVersionStringLabel,
             appVersionLabel,
@@ -103,62 +141,64 @@ final class MyPageView: BaseUIView {
             underLineView
         )
     }
-
+    
     override func setLayout() {
         scrollView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
-
+        
         contentView.snp.makeConstraints {
             $0.edges.equalToSuperview()
             $0.width.equalTo(scrollView)
         }
-
+        
         userImage.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(24)
-            $0.centerX.equalToSuperview()
-            $0.height.width.equalTo(100)
+            $0.top.equalToSuperview().offset(12)
+            $0.leading.equalToSuperview().inset(24)
+            $0.height.width.equalTo(48)
         }
-
+        
         userNicknameLabel.snp.makeConstraints {
-            $0.top.equalTo(userImage.snp.bottom).offset(6)
-            $0.centerX.equalTo(userImage)
-            $0.height.equalTo(40)
+            $0.leading.equalTo(userImage.snp.trailing).offset(12)
+            $0.bottom.equalTo(userImage.snp.centerY)
         }
-
+        
+        userAffiliationLabel.snp.makeConstraints {
+            $0.top.equalTo(userNicknameLabel.snp.bottom)
+            $0.leading.equalTo(userNicknameLabel.snp.leading)
+        }
+        
         myPageTableView.snp.makeConstraints {
-            $0.top.equalTo(userNicknameLabel.snp.bottom).offset(16)
+            $0.top.equalTo(userImage.snp.bottom).offset(24)
             $0.leading.trailing.equalToSuperview()
-            let cellHeight = 60
-            let totalHeight = MyPageLocalData.myPageTableLabelList.count * cellHeight
-            $0.height.equalTo(totalHeight)
+            $0.height.equalTo(myPageTableViewHeight)
             $0.width.equalToSuperview()
         }
-
+        
         appVersionStringLabel.snp.makeConstraints { make in
             make.top.equalTo(myPageTableView.snp.bottom).offset(6)
             make.leading.equalToSuperview().inset(24)
         }
-
+        
         appVersionLabel.snp.makeConstraints { make in
             make.top.equalTo(myPageTableView.snp.bottom).offset(6)
             make.trailing.equalToSuperview().inset(24)
         }
-
+        
         // TODO: withdrawStackView를 프로퍼티로 선언할 때, lazy를 사용하면 레이아웃이 한 타임 늦게 잡히는 문제로 인해서 여기에서 스택 안에 들어갈 뷰를 추가함. 개선 방법이 없는지 확인.
         userWithdrawButton.snp.makeConstraints { make in
             make.top.equalTo(appVersionLabel.snp.bottom).offset(16)
             make.trailing.equalToSuperview().inset(24)
             make.bottom.equalToSuperview().inset(70)
         }
-
+        
         underLineView.snp.makeConstraints {
             $0.top.equalTo(userWithdrawButton.snp.bottom)
             $0.leading.trailing.equalTo(userWithdrawButton)
             $0.height.equalTo(0.5)
         }
     }
-
+    
     private func registerTableViewCells() {
         myPageTableView.register(
             MyPageTableDefaultCell.self,
@@ -169,8 +209,20 @@ final class MyPageView: BaseUIView {
             forCellReuseIdentifier: NotificationSettingTableViewCell.identifier
         )
     }
-
-    public func setUserInfo(nickname: String) {
+    
+    public func setUserInfo(
+        nickname: String,
+        collegeName: String?,
+        departmentName: String?
+    ) {
         userNicknameLabel.text = nickname
+
+        let affiliationText = [collegeName, departmentName]
+            .compactMap { $0 }
+            .filter { !$0.isEmpty }
+            .joined(separator: " ")
+
+        userAffiliationLabel.text = affiliationText
+        userAffiliationLabel.isHidden = affiliationText.isEmpty
     }
 }
--- EATSSU/App/Sources/Presentation/MyPage/ViewController/MyPageViewController.swift
+++ EATSSU/App/Sources/Presentation/MyPage/ViewController/MyPageViewController.swift
@@ -16,20 +16,20 @@ import SnapKit
 
 final class MyPageViewController: BaseViewController {
     // MARK: - Properties
-
+    
     private var nickName = ""
     private var switchState = false
-    private let myPageTableLabelList = MyPageLocalData.myPageTableLabelList
-
+    private let sections = MyPageSectionData.sections
+    
     // MARK: - UI Components
-
+    
     let mypageView = MyPageView()
-
+    
     // MARK: - Life Cycles
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setTableViewDelegate()
         loadSwitchStateFromUserDefaults()
     }
@@ -39,36 +39,43 @@ final class MyPageViewController: BaseViewController {
         
         logScreenView(screenID: FirebaseScreenID.MyPage.mypage1)
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        let userInfo = UserInfoManager.shared.getCurrentUserInfo()
 
-        nickName = UserInfoManager.shared.getCurrentUserInfo()?.nickname ?? TextLiteral.MyPage.unknownUser
-        mypageView.setUserInfo(nickname: nickName)
+        nickName = userInfo?.nickname ?? TextLiteral.MyPage.unknownUser
+
+        mypageView.setUserInfo(
+            nickname: nickName,
+            collegeName: userInfo?.collegeName,
+            departmentName: userInfo?.departmentName
+        )
     }
-
+    
     // MARK: - Functions
-
+    
     override func setCustomNavigationBar() {
         super.setCustomNavigationBar()
         navigationItem.title = TextLiteral.MyPage.myPage
     }
-
+    
     override func configureUI() {
         view.addSubviews(mypageView)
     }
-
+    
     override func setLayout() {
         mypageView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
     }
-
+    
     override func setButtonEvent() {
         mypageView.userWithdrawButton
             .addTarget(self, action: #selector(userWithdrawButtonTapped), for: .touchUpInside)
     }
-
+    
     private func setFirebaseTask() {
         FirebaseRemoteConfig.shared.fetchRestaurantInfo()
     }
@@ -79,70 +86,95 @@ final class MyPageViewController: BaseViewController {
         let userWithdrawViewController = UserWithdrawViewController(nickName: nickName)
         navigationController?.pushViewController(userWithdrawViewController, animated: true)
     }
-
+    
     /// TableViewDelegate & DataSource를 해당 클래스로 할당합니다.
     private func setTableViewDelegate() {
         mypageView.myPageTableView.dataSource = self
         mypageView.myPageTableView.delegate = self
+        
+        if #available(iOS 15.0, *) {
+            mypageView.myPageTableView.sectionHeaderTopPadding = 0
+        }
     }
-
+    
     /// 로그아웃 Alert를 스크린에 표시하는 메소드
     private func logoutShowAlert() {
         let alert = UIAlertController(title: TextLiteral.MyPage.logout,
                                       message: TextLiteral.MyPage.askLogout,
                                       preferredStyle: UIAlertController.Style.alert)
-
+        
         let cancelAction = UIAlertAction(title: TextLiteral.Common.cancelDark,
                                          style: .default,
                                          handler: nil)
-
+        
         let fixAction = UIAlertAction(title: TextLiteral.MyPage.logout,
                                       style: .default,
                                       handler: { _ in
-                                          RealmService.shared.resetDB()
-
-                                          let loginViewController = LoginViewController()
-                                          if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-                                             let keyWindow = windowScene.windows.first(where: { $0.isKeyWindow })
-                                          {
-                                              keyWindow.replaceRootViewController(UINavigationController(rootViewController: loginViewController))
-                                          }
-                                      })
-
+            RealmService.shared.resetDB()
+            
+            let loginViewController = LoginViewController()
+            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+               let keyWindow = windowScene.windows.first(where: { $0.isKeyWindow })
+            {
+                keyWindow.replaceRootViewController(UINavigationController(rootViewController: loginViewController))
+            }
+        })
+        
         alert.addAction(cancelAction)
         alert.addAction(fixAction)
-
+        
         present(alert, animated: true, completion: nil)
     }
-
+    
     /// UserDefaults에 스위치 상태 저장
     private func saveSwitchStateToUserDefaults() {
         print("사용자 푸시 알림 값을 앱 저장소에 보관합니다.")
         UserDefaults.standard.set(switchState, forKey: TextLiteral.MyPage.pushNotificationUserSettingKey)
     }
-
+    
     /// UserDefaults에서 스위치 상태 불러오기
     private func loadSwitchStateFromUserDefaults() {
         print("사용자 푸시 알림 값을 앱 저장소에서 불러옵니다.")
         switchState = UserDefaults.standard.bool(forKey: TextLiteral.MyPage.pushNotificationUserSettingKey)
+    }
+    
+    /// indexPath로 현재 item을 가져오기
+    private func item(at indexPath: IndexPath) -> MyPageLabels {
+        return sections[indexPath.section].items[indexPath.row]
     }
 }
 
 // MARK: - TableView DataSource
 
 extension MyPageViewController: UITableViewDataSource {
-    func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
-        myPageTableLabelList.count
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
     }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.row == MyPageLabels.NotificationSetting.rawValue {
-            let cell = tableView
-                .dequeueReusableCell(
-                    withIdentifier: NotificationSettingTableViewCell.identifier,
-                    for: indexPath
-                ) as! NotificationSettingTableViewCell
+    
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return sections[section].items.count
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        let item = item(at: indexPath)
+        
+        switch item {
+        case .notificationSetting:
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: NotificationSettingTableViewCell.identifier,
+                for: indexPath
+            ) as? NotificationSettingTableViewCell else {
+                return UITableViewCell()
+            }
             
-            // Task로 비동기 작업 처리
+            cell.configure(with: item)
+            
             _Concurrency.Task {
                 let settings = await NotificationManager.shared.checkNotificationSetting()
                 
@@ -157,16 +189,19 @@ extension MyPageViewController: UITableViewDataSource {
                     }
                 }
             }
-            return cell
-        } else {
-            let cell = tableView
-                .dequeueReusableCell(
-                    withIdentifier: MyPageTableDefaultCell.identifier,
-                    for: indexPath
-                ) as! MyPageTableDefaultCell
             
-            let title = myPageTableLabelList[indexPath.row].titleLabel
-            cell.serviceLabel.text = title
+            return cell
+            
+        default:
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: MyPageTableDefaultCell.identifier,
+                for: indexPath
+            ) as? MyPageTableDefaultCell else {
+                return UITableViewCell()
+            }
+            
+            cell.configure(with: item)
+            
             return cell
         }
     }
@@ -175,34 +210,72 @@ extension MyPageViewController: UITableViewDataSource {
 // MARK: - UITableView Delegate
 
 extension MyPageViewController: UITableViewDelegate {
-    func tableView(_: UITableView, heightForRowAt _: IndexPath) -> CGFloat {
-        60
+    func tableView(
+        _ tableView: UITableView,
+        heightForRowAt indexPath: IndexPath
+    ) -> CGFloat {
+        let item = item(at: indexPath)
+        return MyPageTableMetric.rowHeight(for: item)
     }
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+    func tableView(
+        _ tableView: UITableView,
+        heightForHeaderInSection section: Int
+    ) -> CGFloat {
+        return MyPageTableMetric.headerHeight
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        viewForHeaderInSection section: Int
+    ) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = .white
+        
+        let titleLabel = UILabel()
+        titleLabel.text = sections[section].headerTitle
+        titleLabel.font = .caption1
+        titleLabel.textColor = .gray500
+        
+        headerView.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(24)
+            $0.centerY.equalToSuperview()
+        }
+        
+        return headerView
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        didSelectRowAt indexPath: IndexPath
+    ) {
         tableView.deselectRow(at: indexPath, animated: true)
-
-        switch indexPath.row {
-        // "푸시 알림 설정" 스위치 토글
-        case MyPageLabels.NotificationSetting.rawValue:
+        
+        let item = item(at: indexPath)
+        
+        switch item {
+            // "푸시 알림 설정" 스위치 토글
+        case .notificationSetting:
             AnalyticsService.logEvent("click_mypage_menu", parameters: ["menu": "notification_setting"])
             handleNotificationSettingToggle(at: indexPath)
-
-        // "내 정보" 스크린으로 이동
-        case MyPageLabels.MyInfo.rawValue:
+            
+            // "내 정보" 스크린으로 이동
+        case .myInfo:
             AnalyticsService.logEvent("click_mypage_menu", parameters: ["menu": "my_info"])
             let setNickNameVC = SetNickNameViewController()
             setNickNameVC.source = .signup
             navigationController?.pushViewController(setNickNameVC, animated: true)
-
-        // "내 리뷰" 스크린으로 이동
-        case MyPageLabels.MyReview.rawValue:
+            
+            // "내 리뷰" 스크린으로 이동
+        case .myReview:
             AnalyticsService.logEvent("click_mypage_menu", parameters: ["menu": "my_review"])
             let myReviewViewController = MyReviewViewController(nickname: nickName)
             navigationController?.pushViewController(myReviewViewController, animated: true)
-
-        // "문의하기" 스크린으로 이동
-        case MyPageLabels.Inquiry.rawValue:
+            
+            // "문의하기" 스크린으로 이동
+        case .inquiry:
             AnalyticsService.logEvent("click_mypage_menu", parameters: ["menu": "inquiry"])
             TalkApi.shared.chatChannel(channelPublicId: TextLiteral.KakaoChannel.id) { [weak self] error in
                 if error != nil {
@@ -219,34 +292,69 @@ extension MyPageViewController: UITableViewDelegate {
                     // TODO: 카카오톡 채널 채팅방으로 연결 성공했을 때, 앱에서 동작되어야 하는 로직 고민
                 }
             }
-
-        // "서비스 이용약관" 스크린으로 이동
-        case MyPageLabels.TermsOfUse.rawValue:
-            AnalyticsService.logEvent("click_mypage_menu", parameters: ["menu": "terms_of_use"])
-            let provisionViewController = ProvisionViewController(agreementType: .termsOfService)
-            provisionViewController.navigationTitle = TextLiteral.MyPage.termsOfUse
-            navigationController?.pushViewController(provisionViewController, animated: true)
-
-        // "개인정보 이용약관" 스크린으로 이동
-        case MyPageLabels.PrivacyTermsOfUse.rawValue:
-            AnalyticsService.logEvent("click_mypage_menu", parameters: ["menu": "privacy_policy"])
-            let provisionViewController = ProvisionViewController(agreementType: .privacyPolicy)
-            provisionViewController.navigationTitle = TextLiteral.MyPage.privacyTermsOfUse
-            navigationController?.pushViewController(provisionViewController, animated: true)
-
+            
         // "만든사람들" 스크린으로 이동
-        case MyPageLabels.Creator.rawValue:
+        case .creators:
             AnalyticsService.logEvent("click_mypage_menu", parameters: ["menu": "creator"])
             let creatorViewController = CreatorViewController()
             navigationController?.pushViewController(creatorViewController, animated: true)
+            
+        // 잇슈 인스타그램 이동
+        case .instagram:
+            // TODO: 실제 로그 이름 통일
+            //AnalyticsService.logEvent("click_mypage_menu", parameters: ["menu": "instagram"])
+            
+            if let instagramURL = URL(string: "https://www.instagram.com/eatssu.official/") {
+                UIApplication.shared.open(instagramURL)
+            }
+            
+        case .languageSetting:
+            // TODO: 실제 로그 이름 통일
+            // AnalyticsService.logEvent("click_mypage_menu", parameters: ["menu": "language_setting"])
 
-        // "로그아웃" 팝업알림 표시
-        case MyPageLabels.Logout.rawValue:
+            let languageSettingViewController = LanguageSettingViewController()
+            navigationController?.pushViewController(languageSettingViewController, animated: true)
+            
+            // "약관 및 정책" 스크린으로 이동
+        case .termsAndPolicy:
+             let termsAndPolicyViewController = TermsAndPolicyViewController()
+             navigationController?.pushViewController(termsAndPolicyViewController, animated: true)
+            
+            // "로그아웃" 팝업알림 표시
+        case .logout:
             logoutShowAlert()
-
-        default:
-            return
         }
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        heightForFooterInSection section: Int
+    ) -> CGFloat {
+        return section == sections.count - 1 ? CGFloat.leastNormalMagnitude : MyPageTableMetric.footerHeight
+    }
+
+    func tableView(
+        _ tableView: UITableView,
+        viewForFooterInSection section: Int
+    ) -> UIView? {
+        guard section != sections.count - 1 else {
+            return nil
+        }
+        
+        let footerView = UIView()
+        
+        let dividerView = UIView()
+        dividerView.backgroundColor = .gray300
+        
+        footerView.addSubview(dividerView)
+        
+        dividerView.snp.makeConstraints {
+            $0.height.equalTo(1)
+            $0.horizontalEdges.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        
+        return footerView
     }
     
     /// 알림 설정 토글 처리
@@ -272,8 +380,8 @@ extension MyPageViewController: UITableViewDelegate {
                     let formattedDate = dateFormatter.string(from: Date())
                     
                     let message = newState
-                        ? TextLiteral.MyPage.agreeNoti(date: formattedDate)
-                        : TextLiteral.MyPage.disagreeNoti(date: formattedDate)
+                    ? TextLiteral.MyPage.agreeNoti(date: formattedDate)
+                    : TextLiteral.MyPage.disagreeNoti(date: formattedDate)
                     
                     self.showToast(message: message, type: .info)
                 }
@@ -302,7 +410,7 @@ extension MyPageViewController: UITableViewDelegate {
         )
         
         let settingsAction = UIAlertAction(title: TextLiteral.Common.moveToSetting, style: .default) { _ in
-            NotificationManager.shared.openNotificationSettings() 
+            NotificationManager.shared.openNotificationSettings()
         }
         
         let cancelAction = UIAlertAction(title: TextLiteral.Common.cancel, style: .cancel)
